d3e21855949125d4e0fcaccfa02ededf625aa9ff
Alexey Voinov <alexey.v.voinov@gmail.com>
2002-12-02 10:14:38 +0300

    remove old autoconf files
    config.h.in is now always generated by autogen.sh. acconfig.h is not needed for autoconf >= 2.5

--- autogen.sh
+++ autogen.sh
@@ -2,8 +2,10 @@
 ACLOCAL=${ACLOCAL:-aclocal}
 AUTOCONF=${AUTOCONF:-autoconf}
 AUTOMAKE=${AUTOMAKE:-automake}
+AUTOHEADER=${AUTOHEADER:-autoheader}
 $ACLOCAL
 rm -vf config.cache
+$AUTOHEADER
 $AUTOCONF
 $AUTOMAKE --add-missing
 ./configure --enable-pam --with-pamdir=/etc/pam.d $@
