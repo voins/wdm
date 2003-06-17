e049b6458afbd995a3096d27d25ef095ceb53e5d
Alexey Voinov <alexey.v.voinov@gmail.com>
2003-06-17 17:09:52 +0400

    create common parts of interface for wdmPrefs
    buttons like "close" and "save", section selector, should be common for all sections.

--- src/wdmPrefs/prefs.c
+++ src/wdmPrefs/prefs.c
@@ -21,6 +21,13 @@
 #include <wdmlib.h>
 #include <stdlib.h>
 
+typedef struct _wdmPrefsPanel
+{
+	WMWindow *win;
+	WMFrame *fsects;
+} wdmPrefsPanel;
+wdmPrefsPanel wdmPrefs;
+
 void
 closeAction(WMWidget *self, void *data)
 {
@@ -28,10 +35,109 @@ closeAction(WMWidget *self, void *data)
 	exit(0);
 }
 
+static WMFrame *
+CreateSectionsSelector(WMBox *box)
+{
+	WMFrame *frame;
+	WMScrollView *scrollv;
+	WMScroller *scroller;
+
+	frame = WMCreateFrame(box);
+	WMMapWidget(frame);
+	WMSetFrameRelief(frame, WRRaised);
+
+	scrollv = WMCreateScrollView(frame);
+	WMMapWidget(scrollv);
+	WMSetScrollViewRelief(scrollv, WRSunken);
+	WMSetViewExpandsToParent(WMWidgetView(scrollv), 10, 10, 10, 10);
+
+	wdmPrefs.fsects = WMCreateFrame(scrollv);
+	WMResizeWidget(wdmPrefs.fsects, 500, 70);
+	WMMapWidget(wdmPrefs.fsects);
+	WMSetFrameRelief(wdmPrefs.fsects, WRFlat);
+	WMSetScrollViewContentView(scrollv, WMWidgetView(wdmPrefs.fsects));
+
+	WMSetScrollViewHasHorizontalScroller(scrollv, True);
+	WMSetScrollViewHasVerticalScroller(scrollv, False);
+	scroller = WMGetScrollViewHorizontalScroller(scrollv);
+	WMSetScrollerArrowsPosition(scroller, WSANone);
+
+	return frame;
+}
+
+static WMFrame *
+CreateButtons(WMBox *box)
+{
+	WMFrame *fcontrols;
+	WMBox *buttonbox;
+	WMButton *bsave, *bclose;
+
+	fcontrols = WMCreateFrame(box);
+	WMMapWidget(fcontrols);
+	WMSetFrameRelief(fcontrols, WRRaised);
+
+	buttonbox = WMCreateBox(fcontrols);
+	WMMapWidget(buttonbox);
+	WMSetBoxBorderWidth(buttonbox, 0);
+	WMSetViewExpandsToParent(WMWidgetView(buttonbox), 10, 10, 10, 10);
+	WMSetBoxHorizontal(buttonbox, True);
+
+	bsave = WMCreateCommandButton(buttonbox);
+	WMMapWidget(bsave);
+	WMSetButtonText(bsave, "Save");
+	WMAddBoxSubviewAtEnd(buttonbox, WMWidgetView(bsave), False, False, 100, 100, 0);
+
+	bclose = WMCreateCommandButton(buttonbox);
+	WMMapWidget(bclose);
+	WMSetButtonText(bclose, "Close");
+	WMSetButtonAction(bclose, closeAction, NULL);
+	WMAddBoxSubviewAtEnd(buttonbox, WMWidgetView(bclose), False, False, 100, 100, 10);
+
+	return fcontrols;
+}
+
+static WMFrame *
+CreateSections(WMBox *box)
+{
+	WMFrame *frame;
+
+	frame = WMCreateFrame(box);
+	WMMapWidget(frame);
+	WMSetFrameRelief(frame, WRFlat);
+
+	/* TODO: here will be calls to initers
+	 * of various sections with parent = frame. */
+
+	return frame;
+}
+
+static void
+CreatePrefsWindow(WMScreen *scr)
+{
+	WMBox *box;
+
+	wdmPrefs.win = WMCreateWindow(scr, "wdmPrefs");
+	WMSetWindowCloseAction(wdmPrefs.win, closeAction, NULL);
+	WMSetWindowTitle(wdmPrefs.win, "wdmPrefs");
+
+	box = WMCreateBox(wdmPrefs.win);
+	WMMapWidget(box);
+	WMSetBoxBorderWidth(box, 0);
+	WMSetViewExpandsToParent(WMWidgetView(box), 0, 0, 0, 0);
+
+	WMAddBoxSubview(box, WMWidgetView(CreateSectionsSelector(box)),
+			False, False, 113, 113, 0);
+
+	WMAddBoxSubview(box, WMWidgetView(CreateSections(box)),
+			True, True, 0, 0, 0);
+
+	WMAddBoxSubview(box, WMWidgetView(CreateButtons(box)),
+			False, False, 50, 50, 0);
+}
+
 int main(int argc, char *argv[])
 {
 	WMScreen *scr;
-	WMWindow *win;
 
 	WMInitializeApplication("wdmPrefs", &argc, argv);
 	scr = WMOpenScreen(NULL);
@@ -39,13 +145,14 @@ int main(int argc, char *argv[])
 		WDMPanic("could not initialize Screen");
 
 
-	win = WMCreateWindow(scr, "wdmPrefs");
-	WMResizeWidget(win, 600, 300);
-	WMMoveWidget(win, 100, 100);
-	WMRealizeWidget(win);
-	WMMapWidget(win);
-	WMSetWindowCloseAction(win, closeAction, NULL);
-	WMSetWindowTitle(win, "wdmPrefs");
+	CreatePrefsWindow(scr);
+	WMResizeWidget(wdmPrefs.win, 600, 400);
+	WMMoveWidget(wdmPrefs.win, 100, 100);
+
+	WMRealizeWidget(wdmPrefs.win);
+	WMMapSubwidgets(wdmPrefs.win);
+	WMMapWidget(wdmPrefs.win);
+
 	WMScreenMainLoop(scr);
 
 	return 0;
